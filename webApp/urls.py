13a5df6f98282d42c925b726dca5de0cf08d7da1
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-11 19:17:26 +0530

    path regiser added

--- webApp/urls.py
+++ webApp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from user import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/',user_views.register, name='register'),
 ]
