28bbe0820ea77ce635b3cc40d9e38f90fd17ae10
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-11 19:27:17 +0530

    base template added

--- user/views.py
+++ user/views.py
@@ -12,4 +12,4 @@ def register(request):
             return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'user/register.html', {'form': form})
