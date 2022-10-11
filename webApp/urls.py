0436b0303fdfe87ce2fb4c21e84a71173cb9373e
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-11 19:35:02 +0530

    statics

--- webApp/urls.py
+++ webApp/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from user import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',user_views.register, name='register'),
-]
+]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
