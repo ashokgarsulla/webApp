4c8aa7e49382a371a8676ac9af6b5416dd29458f
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-12 23:07:13 +0530

    List view Added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import HttpResponse, render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView,DetailView
+from django.views.generic import CreateView,DetailView,ListView
 from . models import Post
 
 
@@ -21,6 +21,14 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'app/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 2
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'app/post_detail.html'
+
