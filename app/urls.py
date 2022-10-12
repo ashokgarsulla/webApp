494f58a9db8cf9775de3806cd0a1202b5d5ecd93
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-12 15:49:56 +0530

    post creation

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . views import home
+from . views import home,PostCreateView
 urlpatterns = [
-    path('',home,name="apphome")
+    path('',home,name="apphome"),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import HttpResponse, render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
 from . models import Post
+
 
 def home(request):
     context = {
@@ -9,3 +12,11 @@ def home(request):
     template = "app/home.html"
     return render(request,template,context)
 
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'app/post_form.html'
+    fields = ['title', 'content', 'file']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
