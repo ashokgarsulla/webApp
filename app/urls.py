2512525cfd09ef47b96852827600832b0190aeff
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-12 20:04:48 +0530

    detailed views of post

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . views import home,PostCreateView
+from . views import home,PostCreateView,PostDetailView
 urlpatterns = [
     path('',home,name="apphome"),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
