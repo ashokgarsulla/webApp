7abff9667f77b8c166754c1e34a99328b9db5517
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-12 23:33:55 +0530

    User Post view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponse, get_object_or_404, render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView,DetailView,ListView
+from django.contrib.auth.models import User
 from . models import Post
 
 
@@ -27,6 +28,16 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 2
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
