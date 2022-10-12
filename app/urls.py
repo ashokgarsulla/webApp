d0934147ba95fc4e6f0363dea8ec4a3a5aa19dac
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-12 23:46:42 +0530

    post by User Post

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . views import home,PostCreateView,PostDetailView,PostListView
+from . views import UserPostListView,PostCreateView,PostDetailView,PostListView
 urlpatterns = [
     path('',PostListView.as_view(),name="apphome"),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
 ]
