611b76e31fa113f6b6578a1f6a681696426fb305
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-12 13:44:40 +0530

    logout functionality

--- webApp/urls.py
+++ webApp/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',user_views.register, name='register'),
     path('login/',auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
