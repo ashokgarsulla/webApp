5d0cdccc346ba156c142e4daf84ed2ad4f25c2bf
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-12 23:12:32 +0530

    List view and Pagination

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . views import home,PostCreateView,PostDetailView
+from . views import home,PostCreateView,PostDetailView,PostListView
 urlpatterns = [
-    path('',home,name="apphome"),
+    path('',PostListView.as_view(),name="apphome"),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
